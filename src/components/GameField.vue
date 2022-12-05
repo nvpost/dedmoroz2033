c212a63b613c8baf46c214ee95ce16aeaab375b3
nvpost <nvbpost@yandex.ru>
2022-12-05 09:39:29 +0300

    stable

--- src/components/GameField.vue
+++ src/components/GameField.vue
@@ -5,9 +5,11 @@
     <div class="left_side">
         <div class="pribor_item_place">
             <div class="pribor_item" 
+            
                 v-for="(devises_id, key) in level_data.devises_id" :key="key"
                 @drop="drop"
                 @dragover="allowDrop"
+                :id="'wrapper_'+devises_id"
             >
 
                     <img :src="'imgs/pribor/'+devises_id+'.png'" :id="devises_id"
@@ -20,11 +22,14 @@
         </div> 
     </div>
     <div class="right_side">
-        <div class="answer_blok droptarget" data-id="0"
+        <div :class="('answer_blok ' + level_class)" data-id="0"
             @drop="drop"
             @dragover="allowDrop"
         ></div>
-        <div class="answer_blok" data-id="1"></div>
+        <div :class="('answer_blok ' + level_class)" data-id="1"
+            @drop="drop"
+            @dragover="allowDrop"
+        ></div>
     </div>
 </div>
 </template>
@@ -34,14 +39,16 @@ export default{
     name: 'game_field',
     data(){
         return{
-            level_data: this.$store.state.level1
+            level_data: this.$store.state.level1,
+            devise_ids:[],
+            level_class: 'slable'
         }
     },
     methods:{
         dragStart(event)  {
                 event.dataTransfer.setData("id", event.target.id)
                 console.log(event.target.id)
-                console.log('dragStart')
+                // console.log('dragStart')
               },
         dragging(event) {
                 document.getElementById("demo").innerHTML =
@@ -52,11 +59,57 @@ export default{
               },
         drop(event) {
                 event.preventDefault();
-                console.log('dropped')
+
+                
                 var devise_id = event.dataTransfer.getData("id")
-                console.log('devise_id', devise_id)
-                event.target.appendChild(document.getElementById(devise_id));
-              },       
+                // console.log('devise_id', devise_id)
+
+                this.level_class='stable'
+                if(event.target.classList[0]=='answer_blok' 
+                 && !event.target.classList.contains('filled')){
+                    this.devise_ids.push(devise_id)
+
+                    event.target.classList.add('filled')
+                    console.log('addclass', event.target.classList)
+                    event.target.appendChild(document.getElementById(devise_id))
+                    if(this.devise_ids.length==2){
+                        this.check_place()
+                    }
+                }else if(event.target.classList[0]=='pribor_item' ){
+                    event.target.classList.remove('filled')
+                    this.devise_ids.splice(this.devise_ids.indexOf(devise_id), 1)
+                    event.target.appendChild(document.getElementById(devise_id))
+                }
+
+                
+                console.log(this.devise_ids)
+                
+                
+                
+
+              },
+        check_place(){
+            let rules = this.level_data.rule_id
+            let check_flag = this.devise_ids.every(function(element, index) {
+                console.log(index, element)
+                return parseInt(element) === parseInt(rules[index])
+            })
+            console.log(check_flag)
+            if(!check_flag){
+                // this.devise_ids.forEach(i=>{
+                //     document.getElementById('wrapper_'+i).appendChild(document.getElementById(i))
+                // })
+                // this.devise_ids=[]
+                this.level_class='wrong'
+
+            }else{
+                console.log('all_ok')
+                this.level_class='win'
+            }
+            console.log()
+            return check_flag
+        },
+           
     }
     
 }
